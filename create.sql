9e8bc3e85a7b9544a67b571c7662a61a6a04049c
Xenu47 <gelun100@gmail.com>
2020-04-18 02:31:27 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,30 +1,30 @@
 CREATE TABLE Category (
-    app_category VARCHAR(64) NOT NULL PRIMARY KEY
+	app_category VARCHAR(64) NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE Genre (
-    app_genre VARCHAR(255) NOT NULL PRIMARY KEY
+	app_genre VARCHAR(255) NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE Type_ (
-    pricing_type VARCHAR(4) NOT NULL PRIMARY KEY
+	pricing_type VARCHAR(4) NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE App_ (
-    app_name VARCHAR(255) NOT NULL PRIMARY KEY,
-    app_category VARCHAR(64) NOT NULL REFERENCES Category(app_category),
-    app_genre VARCHAR(255) NOT NULL REFERENCES Genre(app_genre)
+	app_name VARCHAR(255) NOT NULL PRIMARY KEY,
+	app_category VARCHAR(64) NOT NULL REFERENCES Category(app_category),
+	app_genre VARCHAR(255) NOT NULL REFERENCES Genre(app_genre)
 );
 
 CREATE TABLE UserInterest (
-    app_name VARCHAR(255) NOT NULL PRIMARY KEY,
-    rating NUMBER(*, 1) NOT NULL,
-    downloads_count VARCHAR(32) NOT NULL,
-    reviews_count NUMBER(16) NOT NULL
+	app_name VARCHAR(255) NOT NULL PRIMARY KEY,
+	rating NUMBER(*, 1) NOT NULL,
+	downloads_count VARCHAR(32) NOT NULL,
+	reviews_count NUMBER(16) NOT NULL
 );
 
 CREATE TABLE Pricing (
-    app_name VARCHAR(255) NOT NULL PRIMARY KEY,
-    pricing_type VARCHAR(50) NOT NULL REFERENCES Type_(pricing_type),
-    price VARCHAR(10) NOT NULL
+	app_name VARCHAR(255) NOT NULL PRIMARY KEY,
+	pricing_type VARCHAR(50) NOT NULL REFERENCES Type_(pricing_type),
+	price VARCHAR(10) NOT NULL
 );
